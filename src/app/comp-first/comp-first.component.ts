f4a99985d31d4dcf7a2cd5b46336d9f9d8a688ee
tafoca <konohamarufotso@gmail.com>
2019-01-12 18:56:53 +0100

    initial architecture of angular

---
+++ src/app/comp-first/comp-first.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-comp-first',
+  templateUrl: './comp-first.component.html',
+  styleUrls: ['./comp-first.component.scss']
+})
+export class CompFirstComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
